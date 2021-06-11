867439a616e768550ab1a3fd8dfc00b5593e423c
Yoomi Kim <yoomi@Yoomis-MacBook-Pro.local>
2021-06-10 23:06:55 -0700

    remove package-lock.json

--- src/apis/medications.js
+++ src/apis/medications.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://med-tracker-apis.herokuapp.com/'
+    // baseURL: 'http://localhost:3001'
 });
